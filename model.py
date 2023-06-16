041f485510db8bfcc02585f02fd3a48f8afa05b1
visay vang <vsvang@gmail.com>
2023-06-16 19:08:28 -0400

    change false to true

--- model.py
+++ model.py
@@ -71,7 +71,7 @@ class Event(db.Model):
 def connect_to_db(flask_app, db_uri="postgresql:///hfbc_church", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
-    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
     db.app = flask_app
     db.init_app(flask_app)
